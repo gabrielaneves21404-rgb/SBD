d478001ff09e47f54508837004959edcf3740276
carolinaraposo25 <carolinaraposo@ua.pt>
2026-05-30 18:33:43 +0100

    Add files via upload

---
+++ sbd2026_112944_115545/Parte III/scripts/Pegar_dados_BD_individual.sql
@@ -0,0 +1,4 @@
+-- Cria a tabela na equipa e puxa os dados da BD individual automaticamente
+SELECT * INTO dbo.DepLocations
+FROM [sbd24_112944].dbo.DepLocations;
+GO
---
+++ sbd2026_112944_115545/Parte III/scripts/Permissão_company.sql
@@ -0,0 +1,4 @@
+
+--Garantir acesso de leitura à tabela dbo.DepLocations para a equipe 09
+
+GRANT SELECT ON dbo.DepLocations TO team_09;
